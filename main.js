303c8c4027e186b8151a973c7ce7de1a225bdb6d
Diavyansh <86036331+Devilalverma@users.noreply.github.com>
2021-09-14 15:26:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+menu_list_array = ["Veg Margherita Pizza","Paneer Tikka Pizza","Veg Extravaganza Pizza","Veg Supreme Pizza","Farm House"];
+                    ;
+
+function getmenu(){
+var htmldata;
+htmldata = "<ol class = 'menulist'>"
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+        // use the sort function as - menu_list_array.sort();
+        menu_list_array.sort()
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+'<div class="card">' +'<img id = "halo" src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
+        }
+        htmldata=htmldata+"</section>"
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+     
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
